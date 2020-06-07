b9fe9ce5376b20b1cdbd91cd2b3b60af148142af
Chris <cdg6285@rit.edu>
2020-06-07 16:28:16 -0400

    Program now watches log directory to parse to html and generate screenshots

---
+++ src/components/report.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./res/config.json', 'utf8'));
+const util = require('util');
+const exec = util.promisify( require( 'child_process' ).exec);
+
+async function runReport(filename) {
+  await parseToHTML(filename);
+  htmlFilename = filename.split('.')[0] + '_wvw_kill.html';
+  await screenshotReport(htmlFilename);
+}
+
+//Uses GW2EI to parse the .evtc file to an html report
+async function parseToHTML(filename) {
+  let command = config.PARSER_EXE + ' -p -c ' + config.PARSER_CONF + ' ' + config.LOG_SAVE_PATH + '\\' + filename;
+  let {stdout, stderr} = await exec(command);
+  console.log(stdout);
+  console.log(stderr);
+}
+
+async function screenshotReport(fp) {
+  //Open HTML file in headless chrome browser
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('file:///' + config.HTML_WATCH_DIR +'/' + fp, {waitUntil: 'networkidle2'});
+  await page.setViewport({
+      width : 1920,
+      height : 1080,
+      deviceScaleFactor : 1
+  })
+  await page.screenshot({path: 'test/out.png'});
+
+  //Screenshot damage
+  await page.evaluate(() => {
+    document.getElementsByClassName("ei-header")[0].style.cssText = 'display : none !important'; //Remove header
+    document.getElementById("actors").style.cssText = 'display : none !important'; //Remove list of players
+  });
+  await page.screenshot({path: 'test/out-damage.png'});
+
+  //Screenshot support
+  await page.evaluate(() => {
+    document.getElementsByClassName("nav-link")[9].click(); //Switch to support view
+  });
+  await page.screenshot({path: 'test/out-support.png'});
+
+
+  await browser.close();
+};
+
+module.exports = {
+  runReport : runReport,
+  parseToHTML : parseToHTML,
+  screenshotReport : screenshotReport,
+}
--- src/discord-evtc.js
+++ src/discord-evtc.js
@@ -1,29 +1,26 @@
-const puppeteer = require('puppeteer');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./res/config.json', 'utf8'));
+const report = require('./components/report');
 
-(async () => {
-  //Open HTML file in headless chrome browser
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('file:///C:/Workspace/discord-evtc/test/med-example.html', {waitUntil: 'networkidle2'});
-  await page.setViewport({
-      width : 1920,
-      height : 1080,
-      deviceScaleFactor : 1
-  })
-
-  //Screenshot damage
-  await page.evaluate(() => {
-    document.getElementsByClassName("ei-header")[0].style.cssText = 'display : none !important'; //Remove header
-    document.getElementById("actors").style.cssText = 'display : none !important'; //Remove list of players
-  });
-  await page.screenshot({path: 'test/out-damage.png'});
-
-  //Screenshot support
-  await page.evaluate(() => {
-    document.getElementsByClassName("nav-link")[9].click(); //Switch to support view
-  });
-  await page.screenshot({path: 'test/out-support.png'});
+const DISCORD_TOKEN = config.DISCORD_TOKEN;
+let processedFiles = new Map();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.login(DISCORD_TOKEN);
 
 
-  await browser.close();
-})();
+
+fs.watch(config.EVTC_WATCH_DIR, async (eventType, filename) => {
+
+  //Make sure we haven't already processed this file
+  if (!(filename in processedFiles) && eventType === 'rename') {
+    processedFiles.set(filename, true);
+    report.runReport(filename);
+  }
+
+});
+
+
