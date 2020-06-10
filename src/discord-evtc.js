2bb9c78c7945851389138d328b2e5924aebec725
Chris <cdg6285@rit.edu>
2020-06-10 19:23:34 -0400

    use chokidar instead of fs.watch to automatically debounce watching a directory for new file creation.

--- src/discord-evtc.js
+++ src/discord-evtc.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const chokidar = require('chokidar');
 const config = JSON.parse(fs.readFileSync('./res/config.json', 'utf8'));
 const report = require('./components/report');
 
@@ -13,12 +14,17 @@ client.login(DISCORD_TOKEN);
 
 
 
-fs.watch(config.EVTC_WATCH_DIR, async (eventType, filename) => {
 
-  console.log(eventType)
-
+chokidar.watch(config.EVTC_WATCH_DIR, {
+  awaitWriteFinish : true,
+  ignoreInitial :  true
+}).on('add', async (path) => {
+  console.log(path);
+  let pathArray = path.split('\\');
+  let filename = pathArray[pathArray.length - 1];
+  console.log(filename);
   //Make sure we haven't already processed this file
-  if (!(filename in processedFiles) && eventType === 'rename') {
+  if (!(filename in processedFiles)) {
     processedFiles.set(filename, true);
     report.runReport(filename, (htmlFilename) => {
       client.channels.fetch("719334198997155960").then( channel => {
